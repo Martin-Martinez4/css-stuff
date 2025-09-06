1ccdcbe4a9caee307922d6222f08d347ed395a2b
Martin-Martinez-4 <martmartez4@gmail.com>
2025-09-05 22:22:36 -0700

    Refactored Javascript to repeat less; Added semantic colors that are fixed; colors change as you move the slider.

--- hsl-color-schemes/script.js
+++ hsl-color-schemes/script.js
@@ -1,44 +1,98 @@
+const Mode = {
+  DARK: "dark",
+  LIGHT: "light"
+}
 let hue = 10;
 let saturation = 10;
+let mode = Mode.DARK;
+
 
 const buttonEl = document.querySelector("button");
 const bodyEl = document.querySelector("body");
 
 const hueSlider = document.querySelector("#hue");
+const hueLabel = document.querySelector("#hue-label")
+
 const saturationSlider = document.querySelector("#saturation");
+const saturationLabel = document.querySelector("#saturation-label")
+
+saturationLabel.textContent = saturation + "% Saturation";
+hueLabel.textContent = hue + " Hue";
+
 
 buttonEl.addEventListener("click", () => {
   bodyEl.classList.toggle("light");
+
+  if(mode == Mode.LIGHT){
+
+    mode = Mode.DARK;
+  }else{
+    mode = Mode.LIGHT;
+  }
+  setThemeVars(hue, saturation, mode);
 })
 
-hueSlider.addEventListener("change", function(){
-  
+hueSlider.addEventListener("input", function () {
+
   hue = this.value;
 
-  bodyEl.style.setProperty("--bg-dark", `hsl(${hue}, ${saturation}%, 2%)`);
-  bodyEl.style.setProperty("--bg", `hsl(${hue}, ${saturation}%, 5%)`);
-  bodyEl.style.setProperty("--bg-light", `hsl(${hue}, ${saturation}%, 10%)`);
-  bodyEl.style.setProperty("--gradient", "linear-gradient(0deg, var(--bg) 95%, var(--bg-light))");
-  bodyEl.style.setProperty("--gradient-hover", "linear-gradient(0deg, var(--bg), var(--bg-light)");
-  bodyEl.style.setProperty("--highlight", `hsl(${hue}, ${saturation}% 30%)`);
-  bodyEl.style.setProperty("--border", `hsl(${hue}, ${saturation}% 30%)`);
-  bodyEl.style.setProperty("--border-card", `solid 1px var(--border)`);
-  bodyEl.style.setProperty("--shadow", ` 0px 2px 2px hsla(${hue}, ${saturation}%, 5%, 0.5), 0px 4px 4px hsla(${hue}, ${saturation}%, 10%, 0.15)`);
+  setThemeVars(hue, saturation, mode);
+
+  hueLabel.textContent = hue + " Hue";
 
 })
 
-saturationSlider.addEventListener("change", function(){
-  
+saturationSlider.addEventListener("input", function () {
+
   saturation = this.value;
 
-    bodyEl.style.setProperty("--bg-dark", `hsl(${hue}, ${saturation}%, 2%)`);
-  bodyEl.style.setProperty("--bg", `hsl(${hue}, ${saturation}%, 5%)`);
-  bodyEl.style.setProperty("--bg-light", `hsl(${hue}, ${saturation}%, 10%)`);
-  bodyEl.style.setProperty("--gradient", "linear-gradient(0deg, var(--bg) 95%, var(--bg-light))");
-  bodyEl.style.setProperty("--gradient-hover", "linear-gradient(0deg, var(--bg), var(--bg-light)");
-  bodyEl.style.setProperty("--highlight", `hsl(${hue}, ${saturation}% 30%)`);
-  bodyEl.style.setProperty("--border", `hsl(${hue}, ${saturation}% 30%)`);
-  bodyEl.style.setProperty("--border-card", `solid 1px var(--border)`);
-  bodyEl.style.setProperty("--shadow", ` 0px 2px 2px hsla(${hue}, ${saturation}%, 5%, 0.5), 0px 4px 4px hsla(${hue}, ${saturation}%, 10%, 0.15)`);
+  setThemeVars(hue, saturation, mode);
+
+  saturationLabel.textContent = saturation + "% Saturation";
 })
+
+function setThemeVars(hue, saturation, mode) {
+
+  let cssVars = {}
+
+
+  switch (mode) {
+    case Mode.DARK:
+      cssVars = {
+        "--bg-dark": `hsl(${hue}, ${saturation}%, 2%)`,
+        "--bg": `hsl(${hue}, ${saturation}%, 5%)`,
+        "--bg-light": `hsl(${hue}, ${saturation}%, 10%)`,
+        "--accent": `hsl(${hue - 60}, ${saturation}%, 12%)`,
+        "--gradient": "linear-gradient(0deg, var(--bg) 95%, var(--bg-light))",
+        "--gradient-hover": "linear-gradient(0deg, var(--bg), var(--bg-light)",
+        "--highlight": `hsl(${hue}, ${saturation}% 30%)`,
+        "--border": `hsl(${hue}, ${saturation}% 30%)`,
+        "--border-card": "solid 1px var(--border)",
+        "--shadow": ` 0px 2px 2px hsla(${hue}, ${saturation}%, 5%, 0.5), 0px 4px 4px hsla(${hue}, ${saturation}%, 10%, 0.15)`
+      }
+      break;
+    case Mode.LIGHT:
+      cssVars = {
+        "--bg-dark": `hsl(${hue}, ${saturation}%, 80%)`,
+        "--bg": `hsl(${hue}, ${saturation}%, 85%)`,
+        "--bg-light": `hsl(${hue}, ${saturation}%, 90%)`,
+        "--accent": `hsl(${hue - 60}, ${saturation}%, 75%)`,
+        "--gradient": "linear-gradient(0deg, var(--bg) 95%, var(--bg-light))",
+        "--gradient-hover": "linear-gradient(0deg, var(--bg), var(--bg-light)",
+        "--highlight": `hsl(${hue}, ${saturation}% 100%)`,
+        "--border": `hsl(${hue}, ${saturation}% 30%)`,
+        "--border-card": "solid 1px var(--bg)",
+        "--shadow": ` 0px 2px 2px hsla(${hue}, ${saturation}%, 10%, 0.5), 0px 4px 4px hsla(${hue}, ${saturation}%, 20%, 0.10)`
+      }
+      break;
+    default:
+
+  }
+
+  for(key in cssVars){
+    console.log(key)
+
+    bodyEl.style.setProperty(key, cssVars[key]);
+  }
+}
 
